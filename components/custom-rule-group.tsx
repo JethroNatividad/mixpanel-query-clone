f7dedc6989000ca4c18f13d557ad9e1e90c9fcff
JethroNatividad <natividad.jet@gmail.com>
2024-09-22 23:24:03 +0800

    feat: add side popover

--- components/custom-rule-group.tsx
+++ components/custom-rule-group.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   RuleGroupBodyComponents,
   RuleGroupHeaderComponents,
   RuleGroupProps,
   useRuleGroup,
+  isFullOptionGroupArray,
+  add,
 } from 'react-querybuilder'
-import { Popover, PopoverContent, PopoverTrigger } from './ui/popover'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+  PopoverClose,
+  PopoverAnchor,
+} from './ui/popover'
 import { Button } from './ui/button'
 import {
   LetterCaseCapitalizeIcon,
@@ -16,6 +24,7 @@ import {
 
 const CustomRuleGroup = (props: RuleGroupProps) => {
   const ruleGroup = useRuleGroup(props)
+  const [isOpen, setIsOpen] = useState(false)
 
   return (
     <div className='border p-4'>
@@ -29,83 +38,111 @@ const CustomRuleGroup = (props: RuleGroupProps) => {
           {...(ruleGroup as Parameters<typeof RuleGroupBodyComponents>[0])}
         />
       </div>
-      <Popover>
+      <Popover open={isOpen} onOpenChange={(e) => setIsOpen(e)}>
         <PopoverTrigger asChild>
           <Button variant='ghost' className='flex font-medium'>
             <PlusIcon className='mr-2' />
             Filter
           </Button>
         </PopoverTrigger>
-        <PopoverContent side='bottom' align='start' className='w-[430px]'>
-          <div className='w-full'>
-            <div className='mb-4 flex'>
-              <div className='flex flex-1 items-center space-x-2 rounded-md bg-muted p-2 text-muted-foreground'>
-                <MagnifyingGlassIcon className='size-5' />
-                <input
-                  type='text'
-                  className='w-full bg-inherit outline-none'
-                  placeholder='Search...'
-                />
+        <PopoverContent side='bottom' align='start' className='w-[430px] p-0'>
+          <Popover open={isOpen}>
+            <PopoverContent side='right' align='start'>
+              <div className='w-[288px]'>
+                <h1>Sidebar</h1>
               </div>
+            </PopoverContent>
+            <PopoverAnchor asChild>
+              <div className='p-4 pb-0'>
+                <div className='mb-4 flex'>
+                  <div className='flex flex-1 items-center space-x-2 rounded-md bg-muted p-2 text-muted-foreground'>
+                    <MagnifyingGlassIcon className='size-5' />
+                    <input
+                      type='text'
+                      className='w-full bg-inherit outline-none'
+                      placeholder='Search...'
+                    />
+                  </div>
 
-              <Button
-                variant='ghost'
-                size='lg'
-                className='flex px-3 font-medium'
-              >
-                <PlusIcon className='mr-2' />
-                Create
-              </Button>
-            </div>
-            <div className='grid grid-cols-10'>
-              <div className='pr col-span-4 border-r pr-2'>
-                <button className='flex w-full items-center justify-start space-x-2 rounded-sm bg-primary/10 p-2 text-primary'>
-                  <LetterCaseCapitalizeIcon className='size-4' />
-                  <span className='text-xs'>All</span>
-                </button>
-              </div>
-              <div className='col-span-6 pl-2'>
-                <p className='p-2 text-xs font-bold text-muted-foreground'>
-                  Recents
-                </p>
-                <button className='flex w-full items-center justify-start space-x-2 rounded-sm bg-primary/10 px-2 py-3 text-primary'>
-                  <LetterCaseCapitalizeIcon className='size-5' />
-                  <span className='text-sm'>All</span>
-                </button>
+                  <Button
+                    variant='ghost'
+                    size='lg'
+                    className='flex px-3 font-medium'
+                  >
+                    <PlusIcon className='mr-2' />
+                    Create
+                  </Button>
+                </div>
+                <div className='grid grid-cols-10'>
+                  <div className='pr col-span-4 border-r pr-2'>
+                    <button className='flex w-full items-center justify-start space-x-2 rounded-sm bg-primary/10 p-2 text-primary'>
+                      <LetterCaseCapitalizeIcon className='size-4' />
+                      <span className='text-xs'>All</span>
+                    </button>
+                  </div>
+                  <div className='col-span-6 pl-2'>
+                    <p className='p-2 text-xs font-bold text-muted-foreground'>
+                      Recents
+                    </p>
+                    <button className='flex w-full items-center justify-start space-x-2 rounded-sm bg-primary/10 px-2 py-3 text-primary'>
+                      <LetterCaseCapitalizeIcon className='size-5' />
+                      <span className='text-sm'>All</span>
+                    </button>
 
-                <hr className='my-2' />
-                <p className='p-2 text-xs font-bold text-muted-foreground'>
-                  All Properties
-                </p>
-                <div>
-                  {ruleGroup.schema.fields.map((field) => {
-                    // if field has fieldType, and === date
-                    if ('inputType' in field && field.inputType === 'date') {
-                      return (
-                        <button
-                          key={field.label}
-                          className='flex w-full items-center justify-start space-x-2 rounded-sm px-2 py-3 hover:bg-primary/10 hover:text-primary'
-                        >
-                          <CalendarIcon className='size-5' />
-                          <span className='text-sm'>{field.label}</span>
-                        </button>
-                      )
-                    }
+                    <hr className='my-2' />
+                    <p className='p-2 text-xs font-bold text-muted-foreground'>
+                      All Properties
+                    </p>
+                    <div>
+                      {!isFullOptionGroupArray(ruleGroup.schema.fields) &&
+                        ruleGroup.schema.fields.map((field) => {
+                          const handleAddRule = () => {
+                            const query = ruleGroup.schema.getQuery()
+                            const rule = ruleGroup.schema.createRule()
+                            const path = ruleGroup.path
+                            rule.field = field.name
+                            rule.operator = '='
 
-                    return (
-                      <button
-                        key={field.label}
-                        className='flex w-full items-center justify-start space-x-2 rounded-sm px-2 py-3 hover:bg-primary/10 hover:text-primary'
-                      >
-                        <LetterCaseCapitalizeIcon className='size-5' />
-                        <span className='text-sm'>{field.label}</span>
-                      </button>
-                    )
-                  })}
+                            if (query) {
+                              ruleGroup.schema.dispatchQuery(
+                                add(query, rule, path)
+                              )
+                            }
+                          }
+
+                          if (field.inputType === 'date') {
+                            return (
+                              <PopoverClose key={field.label} asChild>
+                                <button
+                                  key={field.label}
+                                  onClick={handleAddRule}
+                                  className='flex w-full items-center justify-start space-x-2 rounded-sm px-2 py-3 hover:bg-primary/10 hover:text-primary'
+                                >
+                                  <CalendarIcon className='size-5' />
+                                  <span className='text-sm'>{field.label}</span>
+                                </button>
+                              </PopoverClose>
+                            )
+                          }
+
+                          return (
+                            <PopoverClose key={field.label} asChild>
+                              <button
+                                className='flex w-full items-center justify-start space-x-2 rounded-sm px-2 py-3 hover:bg-primary/10 hover:text-primary'
+                                onClick={handleAddRule}
+                              >
+                                <LetterCaseCapitalizeIcon className='size-5' />
+                                <span className='text-sm'>{field.label}</span>
+                              </button>
+                            </PopoverClose>
+                          )
+                        })}
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
-          </div>
+            </PopoverAnchor>
+          </Popover>
         </PopoverContent>
       </Popover>
     </div>
